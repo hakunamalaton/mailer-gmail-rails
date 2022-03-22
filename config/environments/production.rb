341d81ee97b563639f8bf1dc2ec4bc86411b1045
toan lam <lamtoan557@gmail.com>
2022-03-22 20:32:45 +0700

    Production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,11 +2,21 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'https://rails-gmail.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'https://rails-gmail.herokuapp.com' }
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'https://rails-gmail.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://rails-gmail.herokuapp.com' }
+  config.action_mailer.asset_host = 'https://rails-gmail.herokuapp.com'
+  #config.action_mailer.perform_deliveries = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.config.dig(:account, :user_name),
+    password:             Rails.application.credentials.config.dig(:account, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true
+   }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
