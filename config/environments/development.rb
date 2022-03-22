dad6f957e2aa3c5ea8408b1d088d8bdf597fff14
toan lam <lamtoan557@gmail.com>
2022-03-22 19:58:54 +0700

    deploy tessting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,8 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
-  config.action_mailer.asset_host = 'http://localhost:3000'
+  config.action_mailer.default_url_options = { host: 'https://rails-gmail.herokuapp.com' }
+  config.action_mailer.asset_host = 'https://rails-gmail.herokuapp.com'
   #config.action_mailer.perform_deliveries = true
   
   config.action_mailer.delivery_method = :smtp
