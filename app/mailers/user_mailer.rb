81dfdee91b11dbc2ea44e903d66283ddb1f7d1e9
toan lam <lamtoan557@gmail.com>
2022-03-22 19:20:56 +0700

    First commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+    default from: 'lamtoan557@gmail.com'
+  
+    def welcome_email
+      @user = params[:user]
+      @url  = 'http://localhost:3000'
+      attachments['logo.png'] = File.read(Rails.root.join("app/assets/images/logo.png"))
+      p "Lam thien toan can mot buc anh: #{attachments['logo.png'].url}"
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    end
+  end
+  
