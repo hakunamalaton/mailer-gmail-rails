6d2f42fc01c31d0f8f9fc227be09cc684078c9b9
toan lam <lamtoan557@gmail.com>
2022-04-29 20:53:57 +0700

    Add API for success register!

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < ApplicationMailer
-    default from: 'lamtoan557@gmail.com'
+    default from: 'bk.furniture.hcmut@gmail.com'
   
     def welcome_email
       @user = params[:user]
-      @url  = 'https://rails-gmail.herokuapp.com'
+      @url  = 'http://localhost:3000/'
       attachments.inline['logo.png'] = File.read(Rails.root.join("app/assets/images/logo.png"))
-      p "Lam thien toan can mot buc anh: #{attachments['logo.png'].url}"
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+      mail(to: @user.email, subject: 'Welcome to My BK CSE Furniture Site')
     end
   end
   
