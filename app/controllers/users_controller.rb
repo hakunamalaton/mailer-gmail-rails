98d015dc1279e719effdc471983f32de907f8761
toan lam <lamtoan557@gmail.com>
2022-05-03 12:19:58 +0700

    Modify send email now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
     if @user.save
       # Tell the UserMailer to send a welcome email after save
-      UserMailer.with(user: @user).welcome_email.deliver_later
+      UserMailer.with(user: @user).welcome_email.deliver_now
 
       # format.html { redirect_to(@user, notice: 'User was successfully created.') }
       # format.json { render json: @user, status: :created, location: @user }
